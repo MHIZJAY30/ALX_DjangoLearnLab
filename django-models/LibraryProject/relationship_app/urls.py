6762c64eccf5853c117ce2b73cbd08356ad5a40b
Juliet Duru <julietduru78@gmail.com>
2025-07-18 13:23:40 -0700

    Updated urls.py to use function based register

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -6,11 +6,13 @@ urlpatterns = [
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
 
+
 from django.urls import path
-from .views import RegisterView, CustomLoginView, CustomLogoutView
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]

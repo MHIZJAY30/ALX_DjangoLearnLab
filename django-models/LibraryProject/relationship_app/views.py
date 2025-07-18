c2aa2bfc198f6f03dae5afecdfd31f4d2a3b9660
Juliet Duru <julietduru78@gmail.com>
2025-07-18 07:52:16 -0700

    Changed list_books view to retuen plain text

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
-
+from django.http import HttpResponse
 from .models import Book
 
 def list_books(request):
     books = Book.objects.select_related('author').all()
-    return render(request, 'list_books.html', {'books': books})
+    output = "\n".join([f"{book.title} by {book.author.name}" for book in books])
+    return HttpResponse(output, content_type='text/plain')
+
 
 from django.views.generic.detail import DetailView
 from .models import Library

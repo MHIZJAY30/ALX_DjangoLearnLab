7300e4a7d52baa763c9a17116b7b5ae8e2ab7d85
Juliet Duru <julietduru78@gmail.com>
2025-08-01 11:30:15 -0700

    Implemented authentication and permissions classes to api

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -11,3 +11,14 @@ urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
     path('', include(router.urls)),
 ]
+
+
+from rest_framework.authtoken.views import obtain_auth_token
+
+
+# This endpoint provides token to users via username/password
+urlpatterns = [
+    path('books/', BookList.as_view(), name='book-list'),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),  
+    path('', include(router.urls)),
+]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 from rest_framework import generics
@@ -14,6 +15,9 @@ from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
 
+# BookViewSet handles CRUD operations on Book model.
+# Only authenticated users can access these endpoints.
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated] 

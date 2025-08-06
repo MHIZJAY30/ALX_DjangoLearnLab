125ddb98b75ad64e9494ea3da4869c9db1ea11e6
Juliet Duru <julietduru78@gmail.com>
2025-08-06 08:09:30 -0700

    Updated url patterns to match checker format: books/update and books/delete

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
     path('books/<int:pk>/', BookDetail.as_view(), name='book-detail'),
     path('books/create/', BookCreate.as_view(), name='book-create'),
-    path('books/<int:pk>/update/', BookUpdate.as_view(), name='book-update'),
-    path('books/<int:pk>/delete/', BookDelete.as_view(), name='book-delete'),
+    path('books/update/<int:pk>/', BookUpdate.as_view(), name='book-update'),
+    path('books/delete/<int:pk>/', BookDelete.as_view(), name='book-delete'),
 ]

a4242cb24691c5759845a15946b10084e17baeba
Juliet Duru <julietduru78@gmail.com>
2025-08-16 09:26:42 -0700

    Fixed:Integrated tagging functionality

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from taggit.managers import TaggableManager
 
 # Create your models here.
 class Post(models.Model):
@@ -60,3 +61,7 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
+
+class Post(models.Model):
+    # fields...
+    tags = TaggableManager(blank=True)

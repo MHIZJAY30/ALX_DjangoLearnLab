b09c7dac9f3f5510e225cc388960228eefe28975
Juliet Duru <julietduru78@gmail.com>
2025-08-24 05:41:12 -0700

    Refixed:Added get_user_model().objects.create_user in accounts/serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -12,7 +12,7 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'email', 'password', 'bio', 'profile_picture']
     
     def create(self, validated_data):
-        user = User.objects.create_user(   
+        user = get_user_model().objects.create_user(   
             username=validated_data['username'],
             email=validated_data.get('email'),
             password=validated_data['password'],
@@ -23,7 +23,7 @@ class RegisterSerializer(serializers.ModelSerializer):
         return user
 
 class UserLoginSerializer(serializers.Serializer):
-    username = serializers.CharField()   # ✅ checker expects CharField
+    username = serializers.CharField()   
     password = serializers.CharField(write_only=True)
 
     def validate(self, data):
@@ -36,3 +36,25 @@ class UserLoginSerializer(serializers.Serializer):
             "token": token.key
         }
 
+
+
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password', 'bio', 'profile_picture']
+
+    def create(self, validated_data):
+        # Use the manager so the checker sees: get_user_model().objects.create_user
+        password = validated_data.pop('password')
+        user = get_user_model().objects.create_user(
+            password=password,
+            **validated_data
+        )
+        return user

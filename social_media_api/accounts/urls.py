96e8c4239fb1c150eb411d609ccbfbf83d1d5189
Juliet Duru <julietduru78@gmail.com>
2025-08-24 11:00:38 -0700

    Fixed in the unfollow/<int:user_id>, follow/<int:user_id> in accounts/urls.py

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', CustomObtainAuthToken.as_view(), name='login'),
     path('profile/', ProfileView.as_view(), name='profile'),
-    path('follow/<int:pk>/', FollowUserView.as_view(), name='follow-user'),
-    path('unfollow/<int:pk>/', UnfollowUserView.as_view(), name='unfollow-user'),
+    path('follow/<int:user_id>', FollowUserView.as_view(), name='follow-user'),
+    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow-user'),
     path('following/', views.my_following_list, name='my-following'),
     path('followers/', views.my_followers_list, name='my-followers'),
 ]
